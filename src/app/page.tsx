ace1fb15982137d81ccdd8e42aed2a8751d7cefe
al6862 <al6862@nyu.edu>
2024-07-29 10:28:47 -0400

    made body height svh instead of div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const {description,logo,email} = content
 
   return (
-    <div className="w-screen h-screen px-5 py-5 lg:px-9 lg:py-10 flex flex-col justify-between">
+    <div className="w-full h-full px-5 py-5 lg:px-9 lg:py-10 flex flex-col justify-between">
       <p className="text-xl/close lg:text-5xl/close text-grey-blue">{description}</p>
       <a href={`mailto:${email}`}><Image className="w-full" src={logo.asset.url} alt="Sally-Ann Dale logo" height={logo.asset.metadata.dimensions.height} width={logo.asset.metadata.dimensions.width} /></a>
     </div>
