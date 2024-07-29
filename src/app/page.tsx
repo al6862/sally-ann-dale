05d32a40c850c6114dd2abedd80291625f1e492f
al6862 <al6862@nyu.edu>
2024-07-28 23:49:31 -0400

    added mobile styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default async function Home() {
   const {description,logo,email} = content
 
   return (
-    <div className="w-screen h-screen px-9 py-10 flex flex-col justify-between">
-      <p className="text-5xl text-grey-blue leading-close">{description}</p>
+    <div className="w-screen h-screen px-5 py-5 lg:px-9 lg:py-10 flex flex-col justify-between">
+      <p className="text-xl/close lg:text-5xl/close text-grey-blue">{description}</p>
       <a href={`mailto:${email}`}><Image className="w-full" src={logo.asset.url} alt="Sally-Ann Dale logo" height={logo.asset.metadata.dimensions.height} width={logo.asset.metadata.dimensions.width} /></a>
     </div>
   );
